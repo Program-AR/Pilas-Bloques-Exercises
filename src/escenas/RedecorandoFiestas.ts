15c6b4b1f6047096e21a7b14706abcf54ffa150c
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-07-29 15:25:49 -0300

    Adding Redecorando fiestas exercise

---
+++ src/escenas/RedecorandoFiestas.ts
@@ -0,0 +1,36 @@
+/// <reference path = "UnaFiestaArruinada.ts" />
+
+class RedecorandoFiestas extends UnaFiestaArruinada{
+  matriz(){
+    return [
+      ['F','F','F','T','T','T','T'],
+      ['T','T','T','T','F','T','F'],
+      ['T','F','F','T','F','T','F'],
+      ['T','F','F','T','F','T','F'],
+      ['T','F','F','T','F','F','F']]
+  }
+
+  randomColumn(cantidad){
+    return Math.floor(Math.random() * cantidad)
+  }
+
+  completarConGlobos(){
+    var columnas = 
+      [{columna:0, filas:[1,2,3,4]},
+      {columna:3, filas:[1,2,3,4]},
+      {columna:5, filas:[0,1,2,3]}]
+
+    var index 
+    var columna
+
+    [3, 2].forEach(cantidad => {
+      index = this.randomColumn(cantidad)
+      columna = columnas[index]
+      this.completarColumnaConGlobos(columna.columna, columna.filas)
+      columnas.splice(index, 1)
+    });
+
+    if(Math.random() > 0.5) this.completarColumnaConGlobos(columnas[0].columna, columnas[0].filas)
+
+  }
+}
