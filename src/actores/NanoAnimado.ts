075f502595e67f9b83896dfcb070681877581594
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2022-08-12 12:12:57 -0300

    nano animation

--- src/actores/NanoAnimado.ts
+++ src/actores/NanoAnimado.ts
@@ -2,13 +2,15 @@
 
 class NanoAnimado extends ActorAnimado {
     constructor(x, y) {
-        super(x, y, {grilla: 'nano.png', cantColumnas:14});
+        super(x, y, { grilla: 'nano.png', cantColumnas: 15 });
         this.definirAnimacion('parado', new Cuadros([0]).repetirVeces(30).
-                                          concat([1,1,2,2]).
-                                          concat(new Cuadros([2]).repetirVeces(15)).
-                                          concat([2,2,1,1])
-                                          , 6, true);
-        this.definirAnimacion('correr', [7,9,7,9], 12);
-        this.definirAnimacion('comerBanana', [10,11,12,13,13,12,11,10], 30);
+            concat([1, 1, 2, 2]).
+            concat(new Cuadros([2]).repetirVeces(15)).
+            concat([2, 2, 1, 1])
+            , 6, true);
+        this.definirAnimacion('correr', [7, 9, 7, 9], 12);
+        this.definirAnimacion('comerBanana', [10, 11, 12, 13, 13, 12, 11, 10], 30);
+        this.definirAnimacion('correrChocando', [7, 9, 7, 9, 14, 14, 14, 14, 14, 14], 12);
+        this.definirAnimacion('obstaculo', [14], 12);
     }
 }
