881f724aab3675eb9e866954f23782f7cf25752f
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-08-16 09:35:29 -0300

    fix Recolector de galaxias

--- src/escenas/RecolectorDeGalaxias.ts
+++ src/escenas/RecolectorDeGalaxias.ts
@@ -3,7 +3,7 @@
 /// <reference path = "../habilidades/Flotar.ts" />
 
 class RecolectorDeGalaxias extends ElRecolectorDeEstrellas {
-    dimensionesCuadricula(){
+    dimensionesCuadricula() {
         return {
             alto: 420,
             filas: 7,
@@ -12,7 +12,29 @@ class RecolectorDeGalaxias extends ElRecolectorDeEstrellas {
         }
     }
 
-    filasACompletar() { return [0,1,2,4,5,6] }
+    filasACompletar() { return [0, 1, 2, 4, 5, 6] }
+
+    randomColumn(cantidad) {
+        return Math.floor(Math.random() * cantidad);
+    }
+
+    completarFilaConEstrellas(fila) {
+        var columnas = [1, 2, 3, 4];
+        var index;
+        var columna;
+        [4, 3].forEach(cantidad => {
+            index = this.randomColumn(cantidad);
+            columna = columnas[index];
+            this.agregarEstrella(fila, columna);
+            columnas.splice(index, 1);
+        });
+
+        columnas.forEach(columna => {
+            if (Math.random() > 0.5) {
+                this.agregarEstrella(fila, columna);
+            }
+        })
+    }
 
     ajustarAutomata() {
         this.automata.escala *= 0.7;
