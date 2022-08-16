0aa1d8a82c858c796051e84a34cec17984a1cbd1
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-08-16 09:34:06 -0300

    changing curves - El pasillo curvo de sandias

--- src/escenas/ElPasilloCurvoDeSandias.ts
+++ src/escenas/ElPasilloCurvoDeSandias.ts
@@ -4,19 +4,20 @@
 
 
 class ElPasilloCurvoDeSandias extends MariaLaComeSandias {
-    dimensionesCuadricula(){
+    dimensionesCuadricula() {
         return {
             cantidadColumnas: 4,
             cantidadFilas: 8
         }
     }
 
-    escalaAutomata(){ return 1.9}
+    escalaAutomata() { return 1.9 }
 
     agregarCuadricula() {
         this.cuadricula = new CuadriculaEsparsa(0, 0,
             { alto: 420, ancho: 180, separacionEntreCasillas: 5 },
             { grilla: 'casilla.mariaSandia.png' }, this.matriz())
+        this.colorearCurvas()
     }
 
     matriz() {
@@ -31,30 +32,46 @@ class ElPasilloCurvoDeSandias extends MariaLaComeSandias {
             ['T', 'F', 'F', 'F']]
     }
 
+    colorearCurvas() {
+        const coordenadasCurvas = this.curvasAleatorias().concat(this.extremosCurvas())
+        coordenadasCurvas.forEach(c => this.cuadricula.casilla(c.y, c.x).cambiarImagen('casilla.mariaSandiaCurva.png'))
+    }
+
     completarConSandias() {
         this.completarComienzosDeCurvas()
-        this.completarCurvaAleatoria(2,0)
-        this.completarCurvaAleatoria(1, 4)  
+        this.completarCurvasAleatorias()
     }
 
-    completarComienzosDeCurvas(){
-        [{x: 0, y: 4},
-         {x: 0, y: 6},
-         {x: 1, y: 0},
-         {x: 1, y: 2}
-        ].forEach(({x, y}) => {
-            this.agregarSandia(x,y)
-        })
+    completarComienzosDeCurvas() {
+        this.extremosCurvas().forEach(({ x, y }) => {
+        this.agregarSandia(x, y)
+    })
     }
 
-    completarCurvaAleatoria(x, y){
-        [{x, y},
-         {x: x+1, y},
-         {x: x+1, y: y+1},
-         {x: x+1, y: y+2},
-         {x, y: y+2}
-        ].forEach(({x, y}) => {
-            if(Math.random() > 0.5) this.agregarSandia(x,y)
+    extremosCurvas() {
+        return [{ x: 0, y: 4 },
+        { x: 0, y: 6 },
+        { x: 1, y: 0 },
+        { x: 1, y: 2 }
+        ]
+    }
+
+    coordenadasCurva(x, y) {
+        return [{ x, y },
+        { x: x + 1, y },
+        { x: x + 1, y: y + 1 },
+        { x: x + 1, y: y + 2 },
+        { x, y: y + 2 }
+        ]
+    }
+
+    curvasAleatorias(){
+        return this.coordenadasCurva(2, 0).concat(this.coordenadasCurva(1, 4))
+    }
+
+    completarCurvasAleatorias() {
+        this.curvasAleatorias().forEach(({ x, y }) => {
+            if (Math.random() > 0.5) this.agregarSandia(x, y)
         })
     }
 
