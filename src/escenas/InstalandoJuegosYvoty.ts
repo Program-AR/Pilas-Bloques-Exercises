81b8341340f29cdd5574ee15eebd07a21df11035
Diana López Alvas <dlopezalvas@gmail.com>
2023-05-15 14:40:42 -0300

    instalando juegos yvoty

---
+++ src/escenas/InstalandoJuegosYvoty.ts
@@ -0,0 +1,44 @@
+/// <reference path = "EscenaActividad.ts" />
+/// <reference path = "../actores/Cuadricula.ts" />
+/// <reference path = "../actores/CompuAnimada.ts" />
+/// <reference path = "../actores/InstaladorAnimado.ts" />
+/// <reference path = "../comportamientos/ComportamientoAnimado.ts" />
+/// <reference path = "../comportamientos/MovimientosEnCuadricula.ts" />
+
+class InstalandoJuegosYvoty extends InstalandoJuegos {
+  compus;
+  automata;
+  fondo;
+  cuadricula;
+  estado;
+
+
+  colocarCuadricula(){
+    this.cuadricula = new Cuadricula(0, -20, 1, 4,
+      { ancho: 400, alto: 380 },
+      { grilla: 'casillas.yvoty.png',
+        cantFilas: 1,
+        cantColumnas: 16,
+        bordesDecorados: true,
+        relAspecto: 1,
+      })
+  }
+
+  imagenFondo(){
+    return 'fondo.yvoty.png'
+  }
+
+  colocarAutomata() {
+    this.automata = new Yvoty();
+    this.cuadricula.agregarActor(this.automata, 0, 0);
+		this.automata.escala *= this.escalaSegunCuadricula(1.5);
+		this.automata.setY(0);
+    this.automata.x -= 10
+  }
+
+  ajustarCompu(compu) {
+    compu.escala *= this.escalaSegunCuadricula(0.6)
+    compu.x += 9
+    compu.y += 14
+  }
+}
