4878adde7d6d6cb3685e9aad3803911c1b1f02e9
Diana López Alvas <dlopezalvas@gmail.com>
2023-05-27 00:10:28 -0300

    desaparecer celu

--- src/actores/segundoCiclo/Yvoty/Celular.ts
+++ src/actores/segundoCiclo/Yvoty/Celular.ts
@@ -3,10 +3,11 @@ class Celular extends ActorAnimado {
     static _grilla = 'actor.celular.png'
 
     constructor(prendido = false) {
-        super(0, 0, {cantColumnas: 2});
+        super(0, 0, {cantColumnas: 3});
         this.definirAnimacion("cargado", [1], 1);
         this.definirAnimacion("prendido", [1], 1);
         this.definirAnimacion("descargado", [0], 1);
+        this.definirAnimacion("desaparecer", [2], 1);
 
         if(prendido) this.cargarAnimacion("prendido")
     }
