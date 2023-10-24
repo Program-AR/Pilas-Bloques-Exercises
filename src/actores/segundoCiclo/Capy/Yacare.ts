e420f8700d3437f4e4d4e4478ae3a735014f4a26
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-10-24 17:12:45 -0300

    yacare fix 2

--- src/actores/segundoCiclo/Capy/Yacare.ts
+++ src/actores/segundoCiclo/Capy/Yacare.ts
@@ -4,7 +4,7 @@ class Yacare extends ActorAnimado {
     static _grilla = 'actor.yacare.png'
 
     constructor() {
-        super(0, 0, {cantColumnas: 2});
+        super(0, 0, {cantColumnas: 1});
         this.definirAnimacion("parado", [0], 1);
     }
 }
