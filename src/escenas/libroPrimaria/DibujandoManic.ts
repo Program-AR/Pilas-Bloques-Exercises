64e1cfad15d2271e529ad5df42ce944895eae426
Diana López Alvas <dlopezalvas@gmail.com>
2023-10-31 16:04:59 -0300

    coordenadasAutomata

--- src/escenas/libroPrimaria/DibujandoManic.ts
+++ src/escenas/libroPrimaria/DibujandoManic.ts
@@ -1,4 +1,8 @@
 abstract class DibujandoManic extends DibujandoFiguras {
+
+    xManic: number;
+    yManic: number;
+
     static clasesDeActoresInvolucrados(): typeof ActorAnimado[] {
         return [Manic]
     }
@@ -15,21 +19,30 @@ abstract class DibujandoManic extends DibujandoFiguras {
     crearAutomata() {
         this.automata = new Manic();
         this.automata.escala = 0.8;
-        this.automata.x = -150;
-        this.automata.y = 100;
+        this.automata.x = this.coordenadasAutomata().x;
+        this.automata.y = this.coordenadasAutomata().y;
     }
 
     colorDibujo() {
         return pilas.colores.rgb(230, 240, 6);
     }
 
-    colorDibujoEsperado() {
+    colorDibujoEsperado(){
         return pilas.colores.gris;
+    }
+
+    coordenadasAutomata() {
+        return {x: 0, y: 0}
     }
 }
 
 class DibujandoCuadradoManic extends DibujandoManic {
-    puntosEsperados(){
-      return [{x: -50, y: 50}, {x: -40, y: 50}, {x: -30, y: 50}, {x: -20, y: 50}, {x: -10, y: 50}, {x: 0, y: 50}, {x: 10, y: 50}, {x: 20, y: 50}, {x: 30, y: 50}, {x: 40, y: 50}, {x: 50, y: 50}, {x: 50, y: 40}, {x: 50, y: 30}, {x: 50, y: 20}, {x: 50, y: 10}, {x: 50, y: 0}, {x: 50, y: -10}, {x: 50, y: -20}, {x: 50, y: -30}, {x: 50, y: -40}, {x: 50, y: -50}, {x: 40, y: -50}, {x: 30, y: -50}, {x: 20, y: -50}, {x: 10, y: -50}, {x: 0, y: -50}, {x: -10, y: -50}, {x: -20, y: -50}, {x: -30, y: -50}, {x: -40, y: -50}, {x: -50, y: -50}, {x: -50, y: -40}, {x: -50, y: -30}, {x: -50, y: -20}, {x: -50, y: -10}, {x: -50, y: 0}, {x: -50, y: 10}, {x: -50, y: 20}, {x: -50, y: 30}, {x: -50, y: 40}, {x: -50, y: 50}];
+
+    coordenadasAutomata() {
+        return { x: -50, y: 50 }
     }
-  }
+
+    puntosEsperados() {
+        return [{ x: -50, y: 50 }, { x: -40, y: 50 }, { x: -30, y: 50 }, { x: -20, y: 50 }, { x: -10, y: 50 }, { x: 0, y: 50 }, { x: 10, y: 50 }, { x: 20, y: 50 }, { x: 30, y: 50 }, { x: 40, y: 50 }, { x: 50, y: 50 }, { x: 50, y: 40 }, { x: 50, y: 30 }, { x: 50, y: 20 }, { x: 50, y: 10 }, { x: 50, y: 0 }, { x: 50, y: -10 }, { x: 50, y: -20 }, { x: 50, y: -30 }, { x: 50, y: -40 }, { x: 50, y: -50 }, { x: 40, y: -50 }, { x: 30, y: -50 }, { x: 20, y: -50 }, { x: 10, y: -50 }, { x: 0, y: -50 }, { x: -10, y: -50 }, { x: -20, y: -50 }, { x: -30, y: -50 }, { x: -40, y: -50 }, { x: -50, y: -50 }, { x: -50, y: -40 }, { x: -50, y: -30 }, { x: -50, y: -20 }, { x: -50, y: -10 }, { x: -50, y: 0 }, { x: -50, y: 10 }, { x: -50, y: 20 }, { x: -50, y: 30 }, { x: -50, y: 40 }, { x: -50, y: 50 }];
+    }
+}
